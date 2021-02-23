3f727a29ce8d6ec0c5efba57cb00948ee13738d9
Märt Kalmo <mart.kalmo@gmail.com>
2021-02-23 14:23:27 +0200

    moved some tests

---
+++ tests-project/suite-project.php
@@ -0,0 +1,30 @@
+<?php
+
+$files = scandir(__DIR__);
+
+foreach ($files as $file) {
+    if (!is_file($file)) {
+        continue;
+    } else if (strpos(__FILE__, $file) !== false) {
+        continue;
+    }
+
+    $cmd = sprintf('php %s', $file);
+
+    $output = [];
+
+    exec($cmd, $output);
+
+    $outputString = implode("\n", $output);
+
+    $result = areAllPassed($outputString) ? ' OK' : " NOK";
+
+    printf("%s%s\n", $file, $result);
+}
+
+function areAllPassed(string $output) : bool {
+    preg_match("/(\d+) of (\d+) tests passed./", $output, $matches);
+
+    return count($matches) && $matches[1] == $matches[2];
+}
+
